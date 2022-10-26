a54d19074f04803aa4b9b4cde1fa9b4cc8649810
Rolando Perez <me@rolilink.com>
2022-10-26 18:49:29 -0500

    setting up monorepo

--- src/libs/getApiEndpoint.js
+++
@@ -1,5 +0,0 @@
-const getApiEndpoint = (path = '/') => (
-	`${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}${path}`
-);
-
-export default getApiEndpoint;
